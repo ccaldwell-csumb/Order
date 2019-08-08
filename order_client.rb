46d1c15704cbf116b0e3452682943b9a49570a62
Co-Johnson <cojohnson@csumb.edu>
2019-08-08 23:26:45 +0000

    Order Client Update

--- order_client.rb
+++ order_client.rb
@@ -20,23 +20,49 @@ class OrderClient
   @customer_uri = 'http://localhost:8081'
   @item_uri = 'http://localhost:8082'
 
+ 
+    
+ 
+
   def self.createNewOrder(args)
 
-    body = { email: args[2], firstName: args[1], lastName: args[0] }
-    response = HTTParty.post(
-      'http://localhost:3000/customers',
-      :body => body.to_json,
-      :headers => {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
-    )
+      body = { itemId: args[0], email: args[1]}
+      response = HTTParty.post(
+        @order_uri + '/orders',
+        :body => body.to_json,
+        :headers => {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+      )
+  
+      puts "status code " + response.code.to_s
+      puts response.body
+  end
 
+  def self.retrieveByCustomerEmail(args)
+    body = { email: args[0] }
+    response = HTTParty.get(
+        @order_uri + '/orders',
+        :body => body.to_json,
+        :headers => {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+    )
     puts "status code " + response.code.to_s
     puts response.body
   end
-
-  def self.retrieveBy(args)
-    body = { email: args[0] }
+  
+  def self.retrieveByOrderId(args)
+    body = { id: args[0] }
     response = HTTParty.get(
-        'http://localhost:3000/customers/:email',
+        @order_uri + "/orders/#{args[0]}",
+        :body => body.to_json,
+        :headers => {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+    )
+    puts "status code " + response.code.to_s
+    puts response.body
+  end
+  
+  def self.retrieveByCustomerId(args)
+    body = { customerId: args[0] }
+    response = HTTParty.get(
+        @order_uri + '/orders',
         :body => body.to_json,
         :headers => {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
     )
@@ -44,20 +70,31 @@ class OrderClient
     puts response.body
   end
 
-  def self.registerCustomer(args)
-    body = { id: args[0] }
-    response = HTTParty.get(
-        "http://localhost:3000/customers/#{args[0]}",
+  def self.registerNewCustomer(args)
+    body = { email: args[2], firstName: args[1], lastName: args[0]}
+    response = HTTParty.post(
+        @customer_uri + "/customers",
+        :body => body.to_json,
         :headers => {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
     )
     puts "status code " + response.code.to_s
     puts response.body
   end
   
-  def self.lookupCustomerBy(args)
-    body = { id: args[0] }
+  def self.lookupCustomerById(args)
     response = HTTParty.get(
-        "http://localhost:3000/customers/#{args[0]}",
+        @customer_uri + "/customers/#{args[0]}",
+        :headers => {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+    )
+    puts "status code " + response.code.to_s
+    puts response.body
+  end
+  
+  def self.lookupCustomerByEmail(args)
+    query = { email: args[0] }
+    response = HTTParty.get(
+        @customer_uri + '/customers/email',
+        :query => query,
         :headers => {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
     )
     puts "status code " + response.code.to_s
@@ -65,9 +102,9 @@ class OrderClient
   end
   
   def self.createNewItem(args)
-    body = { id: args[0] }
-    response = HTTParty.get(
-        "http://localhost:3000/customers/#{args[0]}",
+    response = HTTParty.post(
+        @item_uri + '/items',
+        :body => args.to_json,
         :headers => {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
     )
     puts "status code " + response.code.to_s
@@ -75,9 +112,8 @@ class OrderClient
   end
   
   def self.lookupItemById(args)
-    body = { id: args[0] }
     response = HTTParty.get(
-        "http://localhost:3000/customers/#{args[0]}",
+        @item_uri + "/items/#{args[0]}",
         :headers => {'Content-Type' => 'application/json', 'Accept' => 'application/json' }
     )
     puts "status code " + response.code.to_s
@@ -91,22 +127,56 @@ command = ''
 
 while command != 'quit'
 
-  puts 'What do you want to do: register, email, id or quit'
+  puts 'What do you want to do: order, retrieve, register, email, id, create, lookup or quit'
   command = gets.chomp!
 
   case command.downcase
+  when 'order'
+    puts 'enter itemID, and email for new order'
+    args = gets.chomp!.split(' ')
+    OrderClient.createNewOrder(args)
+  when 'retrieve'
+    puts 'Search orders by: id, customerId or email'
+    orderFind = gets.chomp!
+    case orderFind.downcase
+      when 'id'
+        puts 'enter id'
+        args = gets.chomp!
+        OrderClient.retrieveByOrderId(args)
+      when 'customerid'
+        puts 'enter customer id'
+        args = gets.chomp!
+        OrderClient.retrieveByCustomerId(args)
+      when 'email'
+        puts 'enter email'
+        args = gets.chomp!
+        OrderClient.retrieveByCustomerEmail(args)
+    end
   when 'register'
     puts 'enter lastName, firstName and email for new customer'
     args = gets.chomp!.split(' ')
-    OrderClient.registerRequest(args)
+    OrderClient.registerNewCustomer(args)
   when 'email'
     puts 'enter email'
     args = gets.chomp!.split(' ')
-    OrderClient.emailRequest(args)
+    OrderClient.lookupCustomerByEmail(args)
   when 'id'
     puts 'enter id'
     args = gets.chomp!
-    OrderClient.idRequest(args)
+    OrderClient.lookupCustomerById(args)
+  when 'create'
+    args = Hash.new
+    puts 'enter description'
+    args[:description] = gets.chomp!
+    puts 'enter price'
+    args[:price] = gets.chomp!
+    puts 'enter stockQty'
+    args[:stockQty] = gets.chomp!
+    OrderClient.createNewItem(args)
+  when 'lookup'
+    puts 'enter item id'
+    args = gets.chomp!
+    OrderClient.lookupItemById(args)
   when 'quit'
     return
   end
